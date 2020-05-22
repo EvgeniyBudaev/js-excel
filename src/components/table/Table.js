966a6402b46045e1175215f5bf29d34b2c772c1c
Evgeniy  Budaev <ebudaev@iflex.ru>
2020-05-22 08:07:00 +0300

    3. Создаем элемент для ресайза

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,13 @@ import {createTable} from "@/components/table/table.template";
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      // listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+    });
+  }
+
   toHTML() {
-    return createTable()
+    return createTable(20)
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -11,14 +11,21 @@ function toCell() {
 
 function toColumn(col) {
   return `
-    <div class="column">${col}</div>
+    <div class="column">
+      ${col}
+      <div class="col-resize"></div>
+    </div>
   `
 }
 
 function createRow(index, content) {
+  const resize = index ? '<div class="row-resize"></div>' : ''
   return `
     <div class="row">
-      <div class="row-info">${index ? index : ''}</div>
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}
+      </div>
       <div class="row-data">${content}</div>
     </div>
   `
