6622668d594eef21748f20500e792c32f09ece40
Evgeniy  Budaev <ebudaev@iflex.ru>
2020-06-17 10:58:55 +0300

    Routing implemented

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,22 @@
+import {Page} from "@core/Page";
+import {$} from "@core/dom";
+import {createRecordsTable} from "./dashboard.function";
+
+export class DashboardPage extends Page {
+  getRoot() {
+    const now = Date.now().toString()
+    return $.create('div', 'db').html(`
+        <div class="db__header">
+      <h1>Excel Dashboard</h1>
+    </div>
+    <div class="db__new">
+      <div class="db__view">
+        <a class="db__create" href="#excel/${now}">Новая <br /> Таблица</a>
+      </div>
+    </div>
+    <div class="db__table db__view">
+    ${createRecordsTable()}
+    </div>
+    `)
+  }
+}
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,5 @@
-import {storage} from "@core/utils";
 import {defaultStyles, defaultTitle} from "@/constants";
+import {clone} from "@core/utils";
 
 export const defaultState = {
   title: defaultTitle,
@@ -9,6 +9,7 @@ export const defaultState = {
   stylesState: {},
   currentText: '',
   currentStyle: defaultStyles,
+  openedDate: new Date().toJSON()
 }
 
 const normalize = state => ({
@@ -17,6 +18,6 @@ const normalize = state => ({
   currentText: ''
 })
 
-export const initialState = storage('excel-state')
-  ? normalize(storage('excel-state'))
-  : defaultState
+export function normalizeInitialState(state) {
+  return state ? normalize(state) : clone(defaultState)
+}
